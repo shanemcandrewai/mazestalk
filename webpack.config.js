1772c361c0948eac198bbab474c67b1446ad803d
Mcandrew <Shane.Mcandrew@klm.com>
2023-01-15 17:10:31 +0100

    clean up optimization

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const config = {
   entry: resolve(__dirname, 'src/index.js'),
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -26,40 +21,25 @@ const config = {
   devServer: {
     static: resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-
-    ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
-    experiments: {
+
+  experiments: {
     topLevelAwait: true,
-  }
+  },
 };
 
 export default (env, argv) => {
   if (argv.mode === 'development') {
     config.mode = 'development';
     config.devtool = 'source-map';
-  }
-
-  if (argv.mode === 'production') {
+  } else if (argv.mode === 'production') {
     config.mode = 'production';
-    config.optimization = {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-      splitChunks: {
-        chunks: 'all',
-      },
-    };
+    config.optimization.minimize = true;
+    config.optimization.minimizer = [new TerserPlugin()];
   }
-
   return config;
 };
