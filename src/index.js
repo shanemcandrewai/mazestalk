a2526194349a6b053badf0c0c3f21aa2a5660edb
shane <shanedm@protonmail.com>
2023-01-16 09:18:56 +0100

    build maze array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint no-underscore-dangle: ["error", { "allow": ["__dirname"] }] */
 import { FlyControls } from './FlyControls.js';
 
 const {
@@ -16,7 +15,7 @@ const {
   Group,
 } = await import('three');
 
-const getRandomInt = (min, max) => {
+const getRandomInt = (min = 0, max = 2) => {
   // The maximum is exclusive and the minimum is inclusive
   const ceilmin = Math.ceil(min);
   return Math.floor(Math.random() * (Math.floor(max) - ceilmin) + ceilmin);
@@ -24,7 +23,7 @@ const getRandomInt = (min, max) => {
 
 const scene = new Scene();
 scene.background = new Color(0xffffff);
-scene.fog = new Fog(0x002000, 1, 7);
+scene.fog = new Fog(0x002000, 1, 10);
 const camera = new PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -32,8 +31,8 @@ const camera = new PerspectiveCamera(
   1000,
 );
 camera.position.x = 0;
-camera.position.y = 1;
-camera.position.z = 4;
+camera.position.y = 2;
+camera.position.z = 7;
 
 const newDiv = document.createElement('div');
 const newContent = document.createTextNode('');
@@ -65,36 +64,40 @@ const mg = new MeshBasicMaterial({
 const curve90 = new QuadraticBezierCurve3(
   new Vector3(0, 0, 0),
   new Vector3(1, 0, 0),
-  new Vector3(1, 1, 0),
+  new Vector3(1, 2, 0),
 );
 
-const g1 = new TubeGeometry(curve90, 64, 0.1, 16, false);
+const g1 = new TubeGeometry(curve90, 64, 0.1, 8, false);
 const mxp = new Mesh(g1, mg);
 const gra = new Group();
 
-for (let level = 0; level < 2; level += 1) {
-  // const par = ge tRandomInt(nodes);
+const maze = [];
+
+for (let level = 0; level < 3; level += 1) {
   for (let xpos = -level; xpos <= level; xpos += 1) {
-    for (let zpos = -level; zpos <= level; zpos += 1) {
-      if (Math.abs(xpos) + Math.abs(zpos) <= level) {
-        for (let direction = 0; direction < 4; direction += 1) {
+    if (Math.abs(xpos) <= level) {
+      for (let direction = 0; direction < 2; direction += 1) {
+        if (getRandomInt() || level === 0) {
+          maze.push([level, direction, 1]);
           const m = mxp.clone();
-          m.rotateY(Math.PI * 0.5 * direction);
-          m.position.set(xpos, level, zpos);
+          m.rotateY(Math.PI * direction);
+          m.position.set(xpos, level * 2, 0);
           gra.add(m);
+        } else {
+          maze.push([level, direction, 0]);
         }
       }
     }
   }
 }
 
+maze.forEach((it) => { console.log(it); });
 scene.add(gra);
 
-gra.rotateY(Math.PI * 0.1);
 function animate() {
   requestAnimationFrame(animate);
-  // newContent.nodeValue = gra.rotation.y;
-  newContent.nodeValue = ['camera.position.x', camera.position.x,
+  newContent.nodeValue = [
+    'camera.position.x', camera.position.x,
     'camera.position.y', camera.position.y,
     'camera.position.z', camera.position.z,
     'camera.rotation.x', camera.rotation.x,
@@ -102,7 +105,7 @@ function animate() {
     'camera.rotation.z', camera.rotation.z,
   ];
 
-  gra.rotation.y += 0.001;
+  gra.rotation.y += 0.01;
   controls.update(clock.getDelta());
   renderer.render(scene, camera);
 }
