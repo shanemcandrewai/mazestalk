b06a539e49b1a76c5c218cf34d68bf9f34e5dab6
shane <shanedm@protonmail.com>
2023-01-26 21:09:56 +0100

    clean out old code including flycontrols

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
-
 import {
   Scene,
   Color,
-  // Fog,
   PerspectiveCamera,
   WebGLRenderer,
   MeshBasicMaterial,
   QuadraticBezierCurve3,
   TubeGeometry,
   Vector3,
-  // Clock,
   Mesh,
   Group,
   SphereGeometry,
@@ -18,7 +14,6 @@ import {
 
 const scene = new Scene();
 scene.background = new Color(0xffffff);
-// scene.fog = new Fog(0x002000, 1, 10);
 const camera = new PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -29,26 +24,9 @@ camera.position.x = 0;
 camera.position.y = 15;
 camera.position.z = 20;
 
-const newDiv = document.createElement('div');
-const newContent = document.createTextNode('');
-newDiv.appendChild(newContent);
-const id = document.createAttribute('id');
-id.value = 'testdiv';
-newDiv.setAttributeNode(id);
-document.body.appendChild(newDiv);
-
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-// const clock = new Clock();
-
-const controls = new FlyControls(camera, renderer.domElement);
-controls.movementSpeed = 1;
-controls.domElement = renderer.domElement;
-controls.rollSpeed = Math.PI / 24;
-controls.autoForward = false;
-controls.dragToLook = false;
 
 const matGreen = new MeshBasicMaterial({
   color: 0x00ff00,
@@ -103,7 +81,6 @@ for (let y = 0; y < 15; y += curve90.v2.y) {
   }
 }
 // maze.forEach((elem) => { console.log(elem); });
-
 scene.add(group);
 
 const tubePoints = [];
@@ -150,11 +127,7 @@ function animate() {
       }
     }
   } else if (currentStep > 0) { //! chosenBranch
-    console.log('xxx currentStep', currentStep);
-    console.log('xxx chosenBranch', chosenBranch);
-    console.log('xxx sphereStartPos', sphereStartPos);
-    console.log('xxx upperX', upperX);
-    console.log('xxx sphere.position', sphere.position); currentStep -= 1;
+    currentStep -= 1;
     const nextPoint = tubePoints[currentStep].clone();
 
     if (upperX < sphere.position.x) {
@@ -162,21 +135,8 @@ function animate() {
     }
     sphere.position.copy(sphereStartPos).add(nextPoint);
   }
-
   // group.rotation.y += 0.01;
-  // controls.update(clock.getDelta());
-
   renderer.render(scene, camera);
 }
 
 animate();
-
-// newContent.nodeValue = [
-// 'getElapsedTime', clock.getElapsedTime(),
-// 'camera.position.x', camera.position.x,
-// 'camera.position.y', camera.position.y,
-// 'camera.position.z', camera.position.z,
-// 'camera.rotation.x', camera.rotation.x,
-// 'camera.rotation.y', camera.rotation.y,
-// 'camera.rotation.z', camera.rotation.z,
-// ];
