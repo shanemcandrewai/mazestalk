26233898dbf87482ce619e34a7ab6b40603a2c3f
shane <shanedm@protonmail.com>
2023-02-04 22:03:44 +0100

    test progress

--- src/test.js
+++ src/test.js
@@ -6,14 +6,23 @@ const nodes = [
   { startPos: { x: 1, y: 2, z: 0 } }];
 
 const links = [
-  { fromNode: nodes[0], nodes[1] },
-  { fromNode: nodes[0], nodes[2] }];
+  { fromNode: nodes[0], toNode: nodes[1] },
+  { fromNode: nodes[0], toNode: nodes[2] }];
 
-const CountToNodes = (a, e, i) => 2;
-const SproutDecision = (a, e, i) => true;
-const canGrow = (a, e, i) => CountToNodes(a, e, i) < 2 && SproutDecision(a, e, i);
+// const CountToNodes = (a, e, i) => 2;
+// const SproutDecision = (a, e, i) => true;
+// const canGrow = (a, e, i) => CountToNodes(a, e, i) < 2 && SproutDecision(a, e, i);
 
-const red = (arr) => arr.reduce((a, e, i) => { a.push(i, e); return a; }, []);
-let res = [1, 2, 3];
-while (res.length < 7) res = red(res);
-console.log(res);
+const red = (arr) => arr.reduce((a, e, i) => {
+  if (!i) {
+    a.nodes.push({ startPos: { x: -2, y: 4, z: 0 } });
+    a.links.push({ fromNode: nodes[1], toNode: nodes[3] });
+  } else {
+    a.nodes.push(i);
+  }
+  return a;
+}, { nodes, links });
+
+while (nodes.length < 7) red(nodes);
+console.log(nodes);
+console.log(links);
