1e1ce8134e155e451d85a7a1f7c8a98924b5ee51
shane <shanedm@protonmail.com>
2023-02-06 09:01:29 +0100

    test.js wip

--- src/test.js
+++ src/test.js
@@ -1,28 +1,16 @@
 // Generate maze V2
 
-const nodes = [
+const nodesArr = [
   { startPos: { x: 0, y: 0, z: 0 } },
+  { startPos: { x: -1, y: 2, z: 0 } },
   { startPos: { x: -1, y: 2, z: 0 } },
   { startPos: { x: 1, y: 2, z: 0 } }];
 
-const links = [
-  { fromNode: nodes[0], toNode: nodes[1] },
-  { fromNode: nodes[0], toNode: nodes[2] }];
+const linksArr = [
+  { fromNode: nodesArr[0], toNode: nodesArr[1] },
+  { fromNode: nodesArr[0], toNode: nodesArr[2] }];
 
-// const CountToNodes = (a, e, i) => 2;
-// const SproutDecision = (a, e, i) => true;
-// const canGrow = (a, e, i) => CountToNodes(a, e, i) < 2 && SproutDecision(a, e, i);
+const nodes = new Map(nodesArr.map((e) => [JSON.stringify(e), e]));
+const links = new Map(linksArr.map((e) => [JSON.stringify(e), e]));
 
-const red = (arr) => arr.reduce((a, e, i) => {
-  if (!i) {
-    a.nodes.push({ startPos: { x: -2, y: 4, z: 0 } });
-    a.links.push({ fromNode: nodes[1], toNode: nodes[3] });
-  } else {
-    a.nodes.push(i);
-  }
-  return a;
-}, { nodes, links });
-
-while (nodes.length < 7) red(nodes);
-console.log(nodes);
-console.log(links);
+Array.from(nodes).filter((e) => e[1].startPos.x === 0)
