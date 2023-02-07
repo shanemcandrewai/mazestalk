5e4f1a8a2f96f8ea948753f7f56161d30e272a08
shane <shanedm@protonmail.com>
2023-02-07 23:05:57 +0100

    v2 class

--- src/test.js
+++ src/test.js
@@ -1,35 +1,25 @@
 // Generate maze V2
 
-const nodes = [
-  { startPos: { x: 0, y: 0, z: 0 } },
-  { startPos: { x: -1, y: 2, z: 0 } },
-  { startPos: { x: -1, y: 2, z: 0 } },
-  { startPos: { x: 1, y: 2, z: 0 } }];
+class maze {
+  static nodes = [
+    { x: 0, y: 0, z: 0 },
+    { x: -1, y: 2, z: 0 },
+    { x: 1, y: 2, z: 0 }];
 
-const edges = [
-  { fromNode: nodes[0], toNode: nodes[1] },
-  { fromNode: nodes[0], toNode: nodes[2] }];
+  static edges = [
+    { fromNode: maze.nodes[0], toNode: maze.nodes[1] },
+    { fromNode: maze.nodes[0], toNode: maze.nodes[2] }];
 
-const getPointsAbove = ((startPos) => []);
-
-const getPreviousEdges = ((startPos) => []);
-
-const getNextEdges = ((startPos) => []);
-
-const getGrowthChance = ((startPos) => {
-  if (NextEdges(startPos) > 1) return 0;
-  const PreviousEdges = getPreviousEdges(startPos);
+  static getNextNodes(startPos) {
+    return maze.edges.reduce((acc, edge) => {
+      if (edge.fromNode.x === startPos.x
+       && edge.fromNode.y === startPos.y
+       && edge.fromNode.z === startPos.z) { acc.push(edge.toNode); }
+      return acc;
+    }, []);
+  }
 }
-);
 
-const addNextPoint = ((NextPos) => true);
-const addNextEdge = ((StartPos, NextPos) => true);
-
-while (nodes.length < 10) {
-  nodes.reduce((a, e, i) => {
-    if (Math.random() > getGrowthChance(e)) {
-      addNextPoint(e);
-    }
-    return a;
-  }, { nodes, edges });
-}
+console.log(maze.getNextNodes({ x: 0, y: 0, z: 0 }));
+console.log(maze.nodes[0].x);
+console.log(maze.edges[0].fromNode.x);
